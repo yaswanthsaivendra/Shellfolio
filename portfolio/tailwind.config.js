bc52abe777e1e00784eb61dfca86d6e8c37b846d
yaswanthsaivendra <yaswanthsaivendra@gmail.com>
2023-10-04 15:45:28 +0530

    adding

--- portfolio/tailwind.config.js
+++ portfolio/tailwind.config.js
@@ -1,13 +1,16 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [ 
-    './public/**/*.{html,js}'
+  content: [
+    './public/**/*.{html,js}',
   ],
   theme: {
     extend: {
+      colors : {
+        backcolor:"#222f3e",
+        primary : "#008080",
+        secondary : "#808080",
+      }
       
     },
   },
   plugins: [],
 }
-
