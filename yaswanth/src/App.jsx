2bbd82ff7a2aceb8c59882649f0a468d602b6645
yaswanth <yaswanth@getpatronus.com>
2024-12-27 23:44:49 +0530

    adding footer

--- yaswanth/src/App.jsx
+++ yaswanth/src/App.jsx
@@ -1,13 +1,14 @@
 import About from './sections/About'
 import Hero from './sections/Hero'
 import Projects from './sections/Projects'
-
+import Footer from './sections/Footer'
 const App = () => {
   return (
     <div className="bg-bgcolor">
       <Hero />
       <About />
       <Projects />
+      <Footer />
     </div>
   )
 }
---
+++ yaswanth/src/sections/Footer.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+const Footer = () => {
+    return (
+        <footer className="bg-gradient-to-b from-bgcolor to-gray-900 text-white py-6 mt-auto">
+            <div className="container mx-auto px-4">
+                <div className="text-center">
+                    <p className="text-sm">© 2024 Yash V</p>
+                </div>
+            </div>
+        </footer>
+    );
+};
+
+export default Footer;
